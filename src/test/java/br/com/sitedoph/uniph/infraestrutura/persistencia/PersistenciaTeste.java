dd3bf1328a32180d8c0d7f2314baf6537eb5b8af
Daivdson <daivdsonc@gmail.com>
2016-01-30 15:49:29 -0200

    Realizado teste Junit

---
+++ src/test/java/br/com/sitedoph/uniph/infraestrutura/persistencia/PersistenciaTeste.java
@@ -0,0 +1,14 @@
+package br.com.sitedoph.uniph.infraestrutura.persistencia;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class PersistenciaTeste {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
